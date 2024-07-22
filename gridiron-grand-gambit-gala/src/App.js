cebcd56bb3e3372a8e160c9a71a83171236d06dc
Evan Japundza <80544633+evanjapundza@users.noreply.github.com>
2024-07-22 16:52:06 -0400

    fixing eslint

--- gridiron-grand-gambit-gala/src/App.js
+++ gridiron-grand-gambit-gala/src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes.js"
 
 function App() {
